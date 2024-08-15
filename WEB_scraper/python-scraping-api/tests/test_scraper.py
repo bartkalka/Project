5cc1ce14fa34ede92015609eab87a9885f023db7
bartkalka <kalkabartosz@gmail.com>
2024-08-15 14:28:48 +0200

    z

--- WEB_scraper/python-scraping-api/tests/test_scraper.py
+++ WEB_scraper/python-scraping-api/tests/test_scraper.py
@@ -1,4 +1,10 @@
 import unittest
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from scraper import scrape_jobs
 
 class TestScraper(unittest.TestCase):
